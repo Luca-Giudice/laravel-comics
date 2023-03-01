3ff6eda2afc7f15b8801b7b299cb3128b2c5377a
Luca-Giudice <2giudice4@gmail.com>
2023-03-01 13:01:58 +0100

    home_header

---
+++ resources/views/home.blade.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+    @vite('resources/js/app.js')
+</head>
+
+<body>
+    <header>
+        <figure>
+            <img src="{{asset('images/dc-logo.png')}}" alt="">
+        </figure>
+        <ul>
+            <li>
+                <a href="{{ url('/characters') }}">CHARACTERS</a>
+            </li>
+            <li>
+                <a href="{{ url('/comics') }}">COMICS</a>
+            </li>
+            <li>
+                <a href="{{ url('/movies') }}">MOVIES</a>
+            </li>
+            <li>
+                <a href="{{ url('/tv') }}">TV</a>
+            </li>
+            <li>
+                <a href="{{ url('/games') }}">GAMES</a>
+            </li>
+            <li>
+                <a href="{{ url('/collectibles') }}">COLLECTIBLES</a>
+            </li>
+            <li>
+                <a href="{{ url('/videos') }}">VIDEOS</a>
+            </li>
+            <li>
+                <a href="{{ url('/fans') }}">FANS</a>
+            </li>
+            <li>
+                <a href="{{ url('/news') }}">NEWS</a>
+            </li>
+            <li>
+                <a href="{{ url('/shop') }}">SHOP</a>
+            </li>
+            <li>
+                
+            </li>
+        </ul>
+    </header>
+</body>
+
+</html>
--- resources/views/layouts/home.blade.php
+++
@@ -1,7 +0,0 @@
-@extends('layouts.main')
-
-@section('title', 'home')
-
-@section('main-content')
-<h1>Home</h1>
-@endsection
