f4de83ecf7a25367eb25739893b7b52fbcfb3fc8
Luca-Giudice <2giudice4@gmail.com>
2023-03-05 23:58:41 +0100

    end

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,68 +3,156 @@
 
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    @vite('resources/js/app.js');
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>{{ env('APP_NAME') }} | Home </title>
+    <link rel="icon" href="{{ asset('images/favicon.ico') }}">
+
+    @vite('resources/js/app.js')
+
+
 </head>
 
 <body>
     <header>
-    <div class="addictional">
-     <ul>
-     <li>
-        DC POWER VISA
-     </li>
-     <li>
-        ADDITIONAL DC SITES
-     </li>
-     </ul>
-    </div>
-    <div class="navbar container">
-        <figure>
-            <img src="{{asset('images/dc-logo.png')}}" alt="">
-        </figure>
-        <ul>
-            <li>
-                <a href="{{ url('/characters') }}">CHARACTERS</a>
-            </li>
-            <li>
-                <a href="{{ url('/comics') }}">COMICS</a>
-            </li>
-            <li>
-                <a href="{{ url('/movies') }}">MOVIES</a>
-            </li>
-            <li>
-                <a href="{{ url('/tv') }}">TV</a>
-            </li>
-            <li>
-                <a href="{{ url('/games') }}">GAMES</a>
-            </li>
-            <li>
-                <a href="{{ url('/collectibles') }}">COLLECTIBLES</a>
-            </li>
-            <li>
-                <a href="{{ url('/videos') }}">VIDEOS</a>
-            </li>
-            <li>
-                <a href="{{ url('/fans') }}">FANS</a>
-            </li>
-            <li>
-                <a href="{{ url('/news') }}">NEWS</a>
-            </li>
-            <li>
-                <a href="{{ url('/shop') }}">SHOP</a>
-            </li>
-            <li>
-                
-            </li>
-        </ul>
-</div>
+        <div class="info-nav">
+            <div class="container">
+                <h6>DC POWER VISA</h6>
+            </div>
+        </div>
+        <div class="container nav">
+            <figure>
+                <a href="{{ url('/') }}">
+                    <img src="{{ Vite::asset('resources/img/dc-logo.png') }}" alt="logo DC">
+                </a>
+            </figure>
+            <ul>
+                <li><a href="{{ route('characters') }}">CHARACTERS</a></li>
+                <li><a href="{{ route('comics') }}">COMICS</a></li>
+                <li><a href="{{ route('movies') }}">MOVIES</a></li>
+                <li><a href="{{ route('tv') }}">TV</a></li>
+                <li><a href="{{ route('games') }}">GAMES</a></li>
+                <li><a href="{{ route('collectibles') }}">COLLECTIBLES</a></li>
+                <li><a href="{{ route('videos') }}">VIDEOS</a></li>
+                <li><a href="{{ route('fans') }}">FANS</a></li>
+                <li><a href="{{ route('news') }}">NEWS</a></li>
+            </ul>
+        </div>
+        <div class="jumbotron">
+
+        </div>
     </header>
     <main>
-    di
+        <section id="home">
+            <div class="container text-center my-4">
+                <h1>Questa è la nostra Home</h1>
+            </div>
+        </section>
     </main>
+    <footer>
+        <section id="dc-comics-footer">
+            <div class="container menu-footer">
+                <div class="list">
+                    <ul>
+                        <h4>DC COMICS</h4>
+                        <li>
+                            <a href="">Characters</a>
+                        </li>
+                        <li>
+                            <a href="">Comics</a>
+                        </li>
+                        <li>
+                            <a href="">Movies</a>
+                        </li>
+                        <li>
+                            <a href="">TV</a>
+                        </li>
+                        <li>
+                            <a href="">Games</a>
+                        </li>
+                        <li>
+                            <a href="">Videos</a>
+                        </li>
+                        <li>
+                            <a href="">News</a>
+                        </li>
+                        <h4>TITLE</h4>
+                        <li>
+                            <a href="">SHOP</a>
+                        </li>
+                        <li>
+                            <a href="">Shop DC</a>
+                        </li>
+                        <li>
+                            <a href="">Shop DC Collectibles</a>
+                        </li>
+                    </ul>
+                    <ul>
+                        <h4>DC</h4>
+                        <li>
+                            <a href="">Terms Of Use</a>
+                        </li>
+                        <li>
+                            <a href="">Privacy Policy (New)</a>
+                        </li>
+                        <li>
+                            <a href="">Ad Choices</a>
+                        </li>
+                        <li>
+                            <a href="">Advertising</a>
+                        </li>
+                        <li>
+                            <a href="">Jobs</a>
+                        </li>
+                        <li>
+                            <a href="">Subscriptions</a>
+                        </li>
+                        <li>
+                            <a href="">Talen Workshops</a>
+                        </li>
+                        <li>
+                            <a href="">CPSC Certificates</a>
+                        </li>
+                        <li>
+                            <a href="">Ratings</a>
+                        </li>
+                        <li>
+                            <a href="">Shop Helps</a>
+                        </li>
+                        <li>
+                            <a href="">Contact Us</a>
+                        </li>
+
+                    </ul>
+                    <ul>
+                        <h4>SITES</h4>
+                        <li>
+                            <a href="">DC</a>
+                        </li>
+                        <li>
+                            <a href="">MAD Magazine</a>
+                        </li>
+                        <li>
+                            <a href="">DC Kids</a>
+                        </li>
+                        <li>
+                            <a href="">DC Universe</a>
+                        </li>
+                        <li>
+                            <a href="">DC Power Visa</a>
+                        </li>
+
+                    </ul>
+                </div>
+                <div class="logo">
+                    <img src="{{ Vite::asset('resources/img/dc-logo_bg.png') }}" alt="">
+                </div>
+            </div>
+        </section>
+        <section id="sign-footer">
+
+        </section>
+    </footer>
 </body>
 
 </html>
--- resources/views/tv.blade.php
+++ resources/views/tv.blade.php
@@ -2,15 +2,11 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>{{env('APP_NAME')}} | @yield('title')</title>
-    @yield('cdns')
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>DC Comics | Comics </title>
 </head>
 <body>
-    <main>
-        @yield('main-contet')
-    </main>
-    @yield('scripts')
+    <h1>Comics</h1>
 </body>
 </html>
